ad2042b78fb15ab9a30527af13c387c2776d64da
Your Name <you@example.com>
2022-12-08 15:37:22 +0700

    good bad face reg

--- face_tracker/okad.py
+++ face_tracker/okad.py
@@ -8,12 +8,15 @@ import blobconverter
 from multiprocessing import Process, Queue, Manager
 import requests
 import time
+#from rtsp import RTSPServer
+#from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
+#app = FastAPI()
 
 REG_API = "http://192.168.20.150:5000/checkin/insert"
 statusMap = {dai.Tracklet.TrackingStatus.NEW : "NEW", dai.Tracklet.TrackingStatus.TRACKED : "TRACKED", dai.Tracklet.TrackingStatus.LOST : "LOST",dai.Tracklet.TrackingStatus.REMOVED: "REMOVED"}
 
-
+#server = RTSPServer()
 Q = Queue()
 data = Manager().dict()
 def send_reg_api(Q, data):
@@ -91,8 +94,7 @@ detectionNetwork.passthrough.link(objectTracker.inputDetectionFrame)
 detectionNetwork.out.link(objectTracker.inputDetections)
 objectTracker.out.link(trackerOut.input)
 
-
-# Pipeline defined, now the device is connected to
+#def runner():
 with dai.Device(pipeline) as device:
 
     # Start the pipeline
@@ -170,8 +172,118 @@ with dai.Device(pipeline) as device:
                 del data[str(t.id)]
             
         cv2.putText(frame, "NN fps: {:.2f}".format(fps), (2,10), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
-    
+        #server.send_data(frame)
         cv2.imshow("tracker", frame)
+                
 
-        if cv2.waitKey(1) == ord('q'):
-            break
+#from model.base_camera import BaseCamera
+#import base64
+#class Camera(BaseCamera):
+#    def __init__(self):
+#        super().__init__()
+
+#    # over-wride of BaseCamera class frames method
+#    @staticmethod
+#    def frames():
+#    
+#        with dai.Device(pipeline) as device:
+
+#            # Start the pipeline
+#            device.startPipeline()
+
+#            preview = device.getOutputQueue("preview")
+#            tracklets = device.getOutputQueue("tracklets")
+
+#            startTime = time.monotonic()
+#            counter = 0
+#            fps = 0
+#            frame = None
+#            prev_status = None
+
+
+#            while(True):
+#                imgFrame = preview.get()
+#                track = tracklets.get()
+
+#                counter+=1
+#                current_time = time.monotonic()
+#                if (current_time - startTime) > 1 :
+#                    fps = counter / (current_time - startTime)
+#                    counter = 0
+#                    startTime = current_time
+
+#                color = (255, 0, 0)
+#                frame = imgFrame.getCvFrame()
+#                new_frame = frame.copy()
+#                trackletsData = track.tracklets
+#                for t in trackletsData:
+#                    roi = t.roi.denormalize(frame.shape[1], frame.shape[0])
+#                    x1 = int(roi.topLeft().x)
+#                    y1 = int(roi.topLeft().y)
+#                    x2 = int(roi.bottomRight().x)
+#                    y2 = int(roi.bottomRight().y)
+#                    bbox = [x1,y1,x2,y2]
+#                    if str(t.id) in data and  ('lostCnt' not in data[str(t.id)] or data[str(t.id)]["bad"] ) and t.status == dai.Tracklet.TrackingStatus.TRACKED:
+#                        Q.put((new_frame,bbox,str(t.id)))
+#                    #cv2.putText(frame, str(label), (x1 + 10, y1 + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+#                    cv2.putText(frame, f"ID: {[t.id]}", (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+#                    if statusMap[t.status] != "LOST" and statusMap[t.status] != "REMOVED":
+#                        cv2.putText(frame, statusMap[t.status], (x1 + 10, y1 + 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+#                    
+#                    
+#                    
+#                    cv2.rectangle(frame, (x1, y1), (x2, y2), color, cv2.FONT_HERSHEY_SIMPLEX)
+#                    
+#                    #Tracking save
+#                    if t.status == dai.Tracklet.TrackingStatus.NEW:
+#                        data[str(t.id)] = {"bad": False} # Reset
+#                    elif t.status == dai.Tracklet.TrackingStatus.TRACKED:
+#        #                data[str(t.id)]['lostCnt'] = 0
+#                        data[str(t.id)] = {
+#                            **data[str(t.id)],
+#                            'lostCnt': 0
+#                            }
+#                    elif t.status == dai.Tracklet.TrackingStatus.LOST:
+#                        curr = data[str(t.id)]
+#                        curr['lostCnt'] += 1
+#                        data[str(t.id)] = {
+#                            **curr
+#                        }
+#                        # If tracklet has been "LOST" for more than 10 frames, remove it
+#                        if 10 < data[str(t.id)]['lostCnt'] and "lost" not in data[str(t.id)]:
+#                            #node.warn(f"Tracklet {t.id} lost: {data[str(t.id)]['lostCnt']}")
+#        #                    del data[str(t.id)]
+#                            curr = data[str(t.id)]
+#                            curr["lost"] = True
+#                            data[str(t.id)] = {
+#                            **curr
+#                            
+#                            }
+#                    elif (t.status == dai.Tracklet.TrackingStatus.REMOVED) and "lost" not in data[str(t.id)]:
+#                        del data[str(t.id)]
+#                    
+#                cv2.putText(frame, "NN fps: {:.2f}".format(fps), (2,10), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+#                #server.send_data(frame)
+#                cv2.imshow("tracker", frame)
+#                image_buffer = cv2.imencode(".jpg", frame)[1].tobytes()
+#                yield base64.b64encode(image_buffer).decode('utf-8')
+
+                
+
+
+video_controller = Camera()
+# Pipeline defined, now the device is connected to
+@app.websocket("/ws_streaming")
+async def websocket_streaming(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            image = video_controller.frames()
+            if image is not None:
+                await websocket.send_bytes(image)
+    except WebSocketDisconnect:
+        print("Client disconnected")
+    finally:
+        websocket.close()
+
+    
--- tmp.py
+++ tmp.py
@@ -1,61 +1,229 @@
+from pathlib import Path
+import cv2
+import depthai as dai
+import numpy as np
 import time
-msgs = dict()
-node = None
-ImageManipConfig = None
-def add_msg(msg, name, seq = None):
-    global msgs
-    if seq is None:
-        seq = msg.getSequenceNum()
-    seq = str(seq)
-    # node.warn(f"New msg {name}, seq {seq}")
-    # Each seq number has it's own dict of msgs
-    if seq not in msgs:
-        msgs[seq] = dict()
-    msgs[seq][name] = msg
-    # To avoid freezing (not necessary for this ObjDet model)
-    if 15 < len(msgs):
-        node.warn(f"Removing first element! len {len(msgs)}")
-        msgs.popitem() # Remove first element
-def get_msgs():
-    global msgs
-    seq_remove = [] # Arr of sequence numbers to get deleted
-    for seq, syncMsgs in msgs.items():
-        seq_remove.append(seq) # Will get removed from dict if we find synced msgs pair
-        # node.warn(f"Checking sync {seq}")
-        # Check if we have both detections and color frame with this sequence number
-        if len(syncMsgs) == 2: # 1 frame, 1 detection
-            for rm in seq_remove:
-                del msgs[rm]
-            # node.warn(f"synced {seq}. Removed older sync values. len {len(msgs)}")
-            return syncMsgs # Returned synced msgs
+import argparse
+import blobconverter
+from multiprocessing import Process, Queue, Manager
+import requests
+import time
+import base64
+from fastapi import FastAPI,WebSocket, WebSocketDisconnect
+REG_API = "http://192.168.20.150:5000/checkin/insert"
+GOOD_BAD_FACE_API = "https://aiclub.uit.edu.vn/gpu/service/goodbadfaceclassifier/predict_binary"
+app = FastAPI()
+
+def good_bad_face(cropped_image):
+    res = requests.post(
+        url=GOOD_BAD_FACE_API,
+        files=dict(binary_file=cropped_image)
+    )
+
+    res = res.json()
+    
+    
+    if "predicts" in res:
+        return res['predicts'][0]
+    
     return None
-def correct_bb(bb):
-    if bb.xmin < 0: bb.xmin = 0.001
-    if bb.ymin < 0: bb.ymin = 0.001
-    if bb.xmax > 1: bb.xmax = 0.999
-    if bb.ymax > 1: bb.ymax = 0.999
-    return bb
-while True:
-    time.sleep(0.001) # Avoid lazy looping
-    preview = node.io['preview'].tryGet()
-    if preview is not None:
-        add_msg(preview, 'preview')
-    face_dets = node.io['face_det_in'].tryGet()
-    if face_dets is not None:
-        # TODO: in 2.18.0.0 use face_dets.getSequenceNum()
-        passthrough = node.io['passthrough'].get()
-        seq = passthrough.getSequenceNum()
-        add_msg(face_dets, 'dets', seq)
-    sync_msgs = get_msgs()
-    if sync_msgs is not None:
-        img = sync_msgs['preview']
-        dets = sync_msgs['dets']
-        for i, det in enumerate(dets.detections):
-            cfg = ImageManipConfig()
-            correct_bb(det)
-            cfg.setCropRect(det.xmin, det.ymin, det.xmax, det.ymax)
-            # node.warn(f"Sending {i + 1}. det. Seq {seq}. Det {det.xmin}, {det.ymin}, {det.xmax}, {det.ymax}")
-            cfg.setResize(224, 224)
-            cfg.setKeepAspectRatio(False)
-            node.io['manip_cfg'].send(cfg)
-            node.io['manip_img'].send(img)
+
+class JESTION:
+    def __init__(self):
+        self.frame = None
+    def run(self):
+    
+
+        statusMap = {dai.Tracklet.TrackingStatus.NEW : "NEW", dai.Tracklet.TrackingStatus.TRACKED : "TRACKED", dai.Tracklet.TrackingStatus.LOST : "LOST",dai.Tracklet.TrackingStatus.REMOVED: "REMOVED"}
+
+
+        Q = Queue()
+        data = Manager().dict()
+
+        p = Process(target=self.send_reg_api,args=(Q,data))
+        p.start()
+        # Start defining a pipeline
+        pipeline = dai.Pipeline()
+
+        colorCam = pipeline.createColorCamera()
+        detectionNetwork = pipeline.createMobileNetDetectionNetwork()
+        objectTracker = pipeline.createObjectTracker()
+        trackerOut = pipeline.createXLinkOut()
+
+        xlinkOut = pipeline.createXLinkOut()
+
+        xlinkOut.setStreamName("preview")
+        trackerOut.setStreamName("tracklets")
+
+        colorCam.setPreviewSize(1080, 1080)
+        colorCam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+        colorCam.setInterleaved(False)
+        colorCam.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+        #colorCam.setBoardSocket(dai.CameraBoardSocket.RGB)
+        colorCam.setFps(40)
+
+        # setting node configs
+        detectionNetwork.setBlobPath(blobconverter.from_zoo(name="face-detection-retail-0005", shaves=6))
+        detectionNetwork.setConfidenceThreshold(0.5)
+        detectionNetwork.input.setBlocking(False)
+
+        face_det_manip = pipeline.create(dai.node.ImageManip)
+        face_det_manip.initialConfig.setResize(300, 300)
+        face_det_manip.initialConfig.setFrameType(dai.RawImgFrame.Type.RGB888p)
+        colorCam.preview.link(face_det_manip.inputImage)
+        face_det_manip.out.link(detectionNetwork.input)
+
+        # Link plugins CAM . NN . XLINK
+        #colorCam.preview.link(detectionNetwork.input)
+        objectTracker.passthroughTrackerFrame.link(xlinkOut.input)
+
+
+        #objectTracker.setDetectionLabelsToTrack([15])  # track only person
+        # possible tracking types: ZERO_TERM_COLOR_HISTOGRAM, ZERO_TERM_IMAGELESS
+        objectTracker.setTrackerType(dai.TrackerType.ZERO_TERM_COLOR_HISTOGRAM)
+        # take the smallest ID when new object is tracked, possible options: SMALLEST_ID, UNIQUE_ID
+        objectTracker.setTrackerIdAssignmentPolicy(dai.TrackerIdAssignmentPolicy.SMALLEST_ID)
+
+        #if fullFrameTracking:
+        colorCam.preview.link(objectTracker.inputTrackerFrame)
+        #else:
+        #    detectionNetwork.passthrough.link(objectTracker.inputTrackerFrame)
+
+        detectionNetwork.passthrough.link(objectTracker.inputDetectionFrame)
+        detectionNetwork.out.link(objectTracker.inputDetections)
+        objectTracker.out.link(trackerOut.input)
+
+
+        # Pipeline defined, now the device is connected to
+        with dai.Device(pipeline) as device:
+
+            # Start the pipeline
+            device.startPipeline()
+
+            preview = device.getOutputQueue("preview")
+            tracklets = device.getOutputQueue("tracklets")
+
+            startTime = time.monotonic()
+            counter = 0
+            fps = 0
+            frame = None
+            prev_status = None
+
+
+            while(True):
+                imgFrame = preview.get()
+                track = tracklets.get()
+
+                counter+=1
+                current_time = time.monotonic()
+                if (current_time - startTime) > 1 :
+                    fps = counter / (current_time - startTime)
+                    counter = 0
+                    startTime = current_time
+
+                color = (255, 0, 0)
+                frame = imgFrame.getCvFrame()
+                #frame = cv2.resize(frame,(500,500))
+                new_frame = frame.copy()
+                trackletsData = track.tracklets
+                for t in trackletsData:
+                    roi = t.roi.denormalize(frame.shape[1], frame.shape[0])
+                    x1 = int(roi.topLeft().x)
+                    y1 = int(roi.topLeft().y)
+                    x2 = int(roi.bottomRight().x)
+                    y2 = int(roi.bottomRight().y)
+                    bbox = [x1,y1,x2,y2]
+                    if str(t.id) in data and data[str(t.id)]["bad"] == False and t.status == dai.Tracklet.TrackingStatus.TRACKED :
+#                        print(data[str(t.id)]["bad"] )
+                        Q.put((new_frame,bbox,str(t.id)))
+                    #cv2.putText(frame, str(label), (x1 + 10, y1 + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+                    if statusMap[t.status] != "LOST" and statusMap[t.status] != "REMOVED":
+                        cv2.putText(frame, f"ID:{t.id}", (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+                        cv2.putText(frame, statusMap[t.status], (x1 + 10, y1 + 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+                    
+                    
+                    
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), color, cv2.FONT_HERSHEY_SIMPLEX)
+                    
+                    #Tracking save
+                    if t.status == dai.Tracklet.TrackingStatus.NEW:
+                        data[str(t.id)] = {"bad": False} # Reset
+                    elif t.status == dai.Tracklet.TrackingStatus.TRACKED:
+        #                data[str(t.id)]['lostCnt'] = 0
+                        data[str(t.id)] = {
+                            **data[str(t.id)],
+                            'lostCnt': 0
+                            }
+                    elif t.status == dai.Tracklet.TrackingStatus.LOST:
+                        curr = data[str(t.id)]
+                        curr['lostCnt'] += 1
+                        data[str(t.id)] = {
+                            **curr
+                        }
+                        # If tracklet has been "LOST" for more than 10 frames, remove it
+                        if 10 < data[str(t.id)]['lostCnt'] and "lost" not in data[str(t.id)]:
+                            #node.warn(f"Tracklet {t.id} lost: {data[str(t.id)]['lostCnt']}")
+        #                    del data[str(t.id)]
+                            curr = data[str(t.id)]
+                            curr["lost"] = True
+                            data[str(t.id)] = {
+                            **curr
+                            
+                            }
+                    elif (t.status == dai.Tracklet.TrackingStatus.REMOVED) and "lost" not in data[str(t.id)]:
+                        del data[str(t.id)]
+                    
+                cv2.putText(frame, "FPS: {:.2f}".format(fps), (2,10), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+            
+                #cv2.imshow("tracker", frame)
+
+                image_buffer = cv2.imencode(".jpg", frame)[1].tobytes()
+#                self.frame = base64.b64encode(image_buffer).decode('utf-8')
+                self.frame=image_buffer
+    def get(self):
+        return self.frame
+    
+    def send_reg_api(self,Q, data):
+        while True:
+            if Q.empty() :
+                continue
+            FRAME, BBOX, idx = Q.get()
+            cropped = FRAME[BBOX[1]:BBOX[3],BBOX[0]:BBOX[2]]
+            cropped_bytes = cv2.imencode(".jpg",cropped)[1].tobytes()
+            status = good_bad_face(cropped_bytes)
+            if status == 'bad':
+                continue
+            curr = data[str(idx)]
+            curr["bad"] = True
+            data[str(idx)] = {
+                **curr
+            }  
+            res = requests.post(url=REG_API,files=dict(avatar=cropped_bytes))
+                 
+
+video_controller = JESTION()
+#p = Process(target=video_controller.run,args=())
+#p.start()
+from threading import Thread
+import time
+thread = Thread(target=video_controller.run)
+thread.start()
+def gen_frame():
+     """Video streaming generator function."""
+     while True:
+        frame=video_controller.get()
+        if frame is not None:
+            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+     
+from fastapi.responses import StreamingResponse
+@app.get("/streaming")
+async def predict():
+ return StreamingResponse(
+     gen_frame(),
+     media_type="multipart/x-mixed-replace; boundary=frame",
+ )
+
+#print(123123123)
+# Pipeline defined, now the device is connected to
+
+
