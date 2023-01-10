839c0b274eaa5b63c907c8896986c56ce02b2039
hao3830 <21520832@gm.uit.edu.vn>
2023-01-10 12:25:29 +0700

    BUG: fix importing

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from utils.api import get_token, good_bad_face, insert_face, search_face
-from utils.camera import gen_frame
 from utils.configs import get_config, get_api_config, get_settings_config
+from utils.camera import gen_frame
 from utils.logger import BiggerRotatingFileHandler
+from utils.api import get_token, good_bad_face, insert_face, search_face
