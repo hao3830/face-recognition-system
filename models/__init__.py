62f383b8a9e3e63a73ee04b9cc741a1538ed69ce
hao3830 <21520832@gm.uit.edu.vn>
2023-01-13 19:57:04 +0700

    change print err

--- models/__init__.py
+++ models/__init__.py
@@ -7,11 +7,11 @@ from models.depthai import get_pipeline
 face_tracker_controler = FaceTracker()
 
 def auto_restart():
-    while 1:
+    while True:
         try:
             face_tracker_controler.run()
-        except:
-            print("Error")
+        except Exception as err:
+            print(f"err: {err}")
     
 thread = Thread(target=auto_restart)
 thread.start()
