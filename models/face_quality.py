13236d73fe4b5680d9490ff3975540bd0c74d362
hao3830 <21520832@gm.uit.edu.vn>
2023-02-02 17:33:40 +0700

    add threading

--- models/face_quality.py
+++ models/face_quality.py
@@ -3,6 +3,8 @@ import onnxruntime as ort
 import numpy as np
 class FaceQuality:
     def __init__(self, backbone_path, quality_path, confident):
+        sess_opt = ort.SessionOptions()
+        sess_opt.intra_op_num_threads = 3
         self.backbone = ort.InferenceSession(backbone_path)
         self.quality = ort.InferenceSession(quality_path)
         self.confident = confident
@@ -15,8 +17,6 @@ class FaceQuality:
         ccropped = np.array(ccropped, dtype = np.float32)
         ccropped = (ccropped - 127.5) / 128.0
         fc = self.backbone.run(None, {'input': ccropped})
-
-
         pred = self.quality.run(None, {'input': fc[0]})[0][0][0]
 
         if pred < self.confident:
