5d4edf1d86b5c5ea30daea53bd7a8ff9fdf80b3d
Your Name <you@example.com>
2023-02-27 13:37:16 +0700

    .

--- models/face_quality.py
+++ models/face_quality.py
@@ -3,10 +3,8 @@ import onnxruntime as ort
 import numpy as np
 class FaceQuality:
     def __init__(self, backbone_path, quality_path, confident):
-        sess_opt = ort.SessionOptions()
-        sess_opt.intra_op_num_threads = 3
-        self.backbone = ort.InferenceSession(backbone_path)
-        self.quality = ort.InferenceSession(quality_path)
+        self.backbone = ort.InferenceSession(backbone_path,providers=[ 'CUDAExecutionProvider'])
+        self.quality = ort.InferenceSession(quality_path, providers=['CUDAExecutionProvider'])
         self.confident = confident
 
     def predict(self, image):
@@ -14,7 +12,7 @@ class FaceQuality:
         ccropped = resized[...,::-1] # BGR to RGB
         ccropped = ccropped.swapaxes(1, 2).swapaxes(0, 1)
         ccropped = np.reshape(ccropped, [1, 3, 112, 112])
-        ccropped = np.array(ccropped, dtype = np.float32)
+        ccropped = np.array(ccropped, dtype = np.float16)
         ccropped = (ccropped - 127.5) / 128.0
         fc = self.backbone.run(None, {'input': ccropped})
         pred = self.quality.run(None, {'input': fc[0]})[0][0][0]
