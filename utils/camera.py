d2f685d97f88fb19f998898eeccecd62aa83648a
hao3830 <21520832@gm.uit.edu.vn>
2023-04-03 13:53:37 +0700

    test

--- utils/camera.py
+++ utils/camera.py
@@ -1,6 +1,6 @@
 import asyncio
 import time
-def gen_frame(video_controller,is_default):
+async def gen_frame(video_controller,is_default):
      """Video streaming generator function."""
      while True:
         if is_default:
@@ -12,7 +12,7 @@ def gen_frame(video_controller,is_default):
 #            image_buffer = cv2.imencode(".jpg", frame)[1].tobytes()
             yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
         
-        time.sleep(1/20)
+        time.sleep(1/30)
 
 async def streamer(gen):
     try:
